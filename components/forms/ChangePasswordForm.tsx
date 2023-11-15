410f7dda6df96b9af825cd18c28c53ff92d3ac3e
Vitalie Plesco <v.plesco@gmail.com>
2023-11-15 11:29:55 +0200

    add change password form component

---
+++ components/forms/ChangePasswordForm.tsx
@@ -0,0 +1,105 @@
+"use client";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { TChangePasswordSchema } from "@/lib/validations/auth";
+import { changePasswordSchema } from "@/lib/validations/auth";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function ChangePasswordForm() {
+  const router = useRouter();
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<TChangePasswordSchema>({
+    resolver: zodResolver(changePasswordSchema),
+  });
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const onSubmit = async (data: any) => {
+    const response = await fetch("/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: data.email,
+        password: data.password,
+      }),
+    });
+
+    if (response.ok) {
+      reset();
+      router.push("/signin");
+    }
+  };
+  return (
+    <Box sx={{ margin: "0 auto" }}>
+      <Box
+        onSubmit={handleSubmit(onSubmit)}
+        component="form"
+        noValidate
+        autoComplete="off"
+      >
+        <Stack spacing={2}>
+          <Typography variant="h6" sx={{ marginTop: "2px" }}>
+            Enter your new password
+          </Typography>
+
+          <TextField
+            error={errors.password ? true : false}
+            type={showPassword ? "text" : "password"}
+            label="Password"
+            {...register("password", { required: "Must be 8 or more" })}
+            variant="outlined"
+            helperText={errors.password ? `${errors.password.message}` : " "}
+          />
+          <TextField
+            error={errors.confirmPassword ? true : false}
+            type={showPassword ? "text" : "password"}
+            label="Confirm Password"
+            {...register("confirmPassword", { required: "Must be 8 or more" })}
+            variant="outlined"
+            helperText={
+              errors.confirmPassword ? `${errors.confirmPassword.message}` : " "
+            }
+          />
+
+          <FormControlLabel
+            control={
+              <Checkbox value="toggle password visibility" color="primary" />
+            }
+            checked={showPassword}
+            onClick={handleClickShowPassword}
+            label="Show password"
+          />
+
+          <Button
+            disabled={isSubmitting}
+            type="submit"
+            size="large"
+            variant="contained"
+            sx={{
+              color: "white",
+            }}
+          >
+            Update password
+          </Button>
+        </Stack>
+      </Box>
+    </Box>
+  );
+}
